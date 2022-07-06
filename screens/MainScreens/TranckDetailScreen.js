93ade718ede779933db9a46201cc53bb0a868b46
Divyanshu-Rout <divyanshukumarrout@gmail.com>
2022-07-07 00:51:37 +0530

    screens added

---
+++ screens/MainScreens/TranckDetailScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TranckDetailScreen = () => {
+    return (
+        <View>
+            <Text>TranckDetailScreen</Text>
+        </View>
+    )
+}
+
+export default TranckDetailScreen
